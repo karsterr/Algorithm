e55106146aa0e1d7baff357e362e8ff31bfffe0e
karsterr <efecan_kara@tuta.io>
2023-12-16 16:25:58 +0300

    Add average example

--- Average/main.c
+++ Average/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int x, y;
+
+    printf("First Number > ");
+    scanf("%d", &x);
+    printf("Second Number > ");
+    scanf("%d", &y);
+
+    double average = (x + y) / 2;
+    printf("Average : %f", average);
+
     return 0;
 }
